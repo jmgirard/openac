ebb59e816d23ec9fee86e515ab4d615182eb194c
jmgirard <me@jmgirard.com>
2024-11-27 18:53:52 +0000

    add whisper functions

--- R/ffmpeg.R
+++ R/ffmpeg.R
@@ -16,53 +16,6 @@ ffprobe <- function(command) {
   system(paste0('"', find_ffprobe(), '" ', command), intern = TRUE)
 }
 
-# extract_wav() ---------------------------------------------------------
-
-#' Extract audio for Whisper
-#'
-#' Extract an audio stream from a video file and transcode it for Whisper.
-#'
-#' @param infile A required string indicating the filepath of the video file.
-#' @param outfile A required string indicating the filepath of the audio file to
-#'   create.
-#' @param stream An optional nonnegative integer indicating which audio stream
-#'   to extract. Note that ffmpeg uses zero-indexing, so the first stream is 0.
-#' @param check An optional logical indicating whether to check if `stream` is
-#'   possible.
-#' @param options An optional string indicating additional options to include in
-#'   the ffmpeg command (after "-i").
-#' @return A string containing the output from ffmpeg.
-#' @export
-extract_wav <- function(infile,
-                        outfile,
-                        stream = 0,
-                        check = TRUE,
-                        options = "") {
-
-  stopifnot(is.character(infile), length(infile) == 1, file.exists(infile))
-  stopifnot(is.character(outfile), length(outfile) == 1)
-  stopifnot(is.numeric(stream) && length(stream) == 1)
-  stopifnot(stream >= 0 && ceiling(stream) == floor(stream))
-  stopifnot(is.logical(check) && length(check) == 1)
-  stopifnot(is.character(options), length(options) == 1)
-
-  if (check) {
-    stopifnot((stream + 1) <= count_audio_streams(infile))
-  }
-
-  command <- paste0(
-    '-i "', infile, '" ',
-    options,
-    ' -map 0:a:', stream,
-    ' -ar 16000', # set sample rate to 16kHz
-    ' -ac 1', # set to mono audio (1 channel)
-    ' -c:a pcm_s16le', # set to 16-bit PCM codec
-    ' "', outfile, '"'
-  )
-  ffmpeg(command)
-}
-
-#' @export
 count_audio_streams <- function(infile) {
   command <- paste0(
     '-v error -select_streams a -show_entries stream=index -of csv=p=0 "',
@@ -73,15 +26,37 @@ count_audio_streams <- function(infile) {
   length(out)
 }
 
+determine_type <- function(infile) {
+  command <- paste0(
+    '-v error',
+    ' -select_streams v:0',
+    ' -show_entries stream=codec_type',
+    ' -of csv=p=0',
+    ' "', infile, '"'
+  )
+  vcheck <- length(ffprobe(command)) > 0
+
+  command <- paste0(
+    '-v error',
+    ' -select_streams a:0',
+    ' -show_entries stream=codec_type',
+    ' -of csv=p=0',
+    ' "', infile, '"'
+  )
+  acheck <- length(ffprobe(command)) > 0
+
+  c(Video = vcheck, Audio = acheck)
+}
+
 # check_ffmpeg() ------------------------------------------------------------
 
 #' @export
 check_ffmpeg <- function() {
 
-  # Try to find the opensmile executable
-  of <- find_ffmpeg()
+  # Try to find the ffmpeg executable
+  ffm <- find_ffmpeg()
 
-  if (is.null(of)) {
+  if (is.null(ffm)) {
     return(FALSE)
   }
 
---
+++ R/whisper.R
@@ -0,0 +1,198 @@
+# whisper_check_audio() ---------------------------------------------------
+
+#' Check if an audio file is ready for analysis by Whisper
+#'
+#' Check if an audio file has the proper format for Whisper, i.e., the pcm_s16le
+#' audio codec, a sampling rate of 16000, and 1 audio channel
+#'
+#' @param infile A required string indicating the filepath of the audio file to
+#'   check.
+#' @param verbose An optional logical indicating whether to print warnings.
+#' @return A logical indicating whether `infile` is ready for whisper.
+#' @export
+whisper_check_audio <- function(infile, verbose = FALSE) {
+
+  stopifnot(is.character(infile), length(infile) == 1, file.exists(infile))
+  stopifnot(is.logical(verbose) && length(verbose) == 1)
+
+  type <- determine_type(infile)
+  streams <- count_audio_streams(infile)
+
+  # Create ffprobe command
+  command <- paste0(
+    '-v error',
+    ' -show_entries stream=codec_name,sample_rate,channels',
+    ' -of default=noprint_wrappers=1:nokey=1',
+    ' "', infile, '"'
+  )
+
+  # Run ffprobe command
+  dat <- ffprobe(command)
+
+  # Check ffprobe output
+  tests <- c(
+    No_Video = type[[1]] == FALSE,
+    One_Stream = streams == 1,
+    Right_Codec = dat[[1]] == "pcm_s16le",
+    Sample_Rate = dat[[2]] == "16000",
+    One_Channel = dat[[3]] == "1"
+  )
+
+  # If verbose, state the result
+  if (verbose) print(tests)
+
+  # Return single logical
+  all(tests)
+}
+
+# whisper_prepare_audio() -------------------------------------------------
+
+#' Prepare an audio stream for analysis by Whisper
+#'
+#' If provided an audio file, convert the specified audio stream to the proper
+#' format for Whisper. Or, if provided a video file, extract the specified audio
+#' stream and convert it to the proper format for Whisper.
+#'
+#' @param infile A required string indicating the filepath to the input audio or
+#'   video file containing the stream to convert or extract.
+#' @param outfile A required string indicating the filepath to the audio (.wav)
+#'   file to create containing only the specified audio stream from `infile`.
+#' @param stream An optional number indicating the index of the audio stream in
+#'   `infile` to convert or extract. Note that ffmpeg uses zero-indexing so the
+#'   default of 0 is the first stream (default = 0).
+#' @return A string containing the text output from ffmpeg.
+#' @export
+whisper_prepare_audio <- function(infile,
+                                  outfile,
+                                  stream = 0) {
+
+  stopifnot(is.character(infile), length(infile) == 1, file.exists(infile))
+  stopifnot(is.character(outfile), length(outfile) == 1)
+  stopifnot(is.numeric(stream) && length(stream) == 1)
+  stopifnot(stream >= 0 && ceiling(stream) == floor(stream))
+
+  # Check that the requested audio stream exists
+  stopifnot((stream + 1) <= count_audio_streams(infile))
+
+  # ...Create ffmpeg command to extract wav
+  command <- paste0(
+    '-y -i "', infile, '" ',
+    ' -map 0:a:', stream,
+    ' -ar 16000', # set sample rate to 16kHz
+    ' -ac 1', # set to mono audio (1 channel)
+    ' -c:a pcm_s16le', # set to 16-bit PCM codec
+    ' "', outfile, '"'
+  )
+  ffmpeg(command)
+
+}
+
+# whisper_transcribe() ----------------------------------------------------
+
+#' Transcribe an audio stream using the specified Whisper model
+#'
+#' If provided a pre-prepared wav file, will directly transcribe it. But if
+#' provided an unsupported audio format or video file, will extract and convert
+#' the specified audio stream before transcribing it. Set `wavfile` if you want
+#' to retain this conversion or leave it NULL to create and later discard a
+#' temporary wav file.
+#'
+#' @param infile A required string indicating the file path to an audio or video
+#'   file containing an audio stream to transcribe.
+#' @param model A required model object produced by `audio.whisper::whisper()`.
+#' @param language The language of the audio. Defaults to 'auto'. For a list of
+#'   all languages the model can handle: see
+#'   `audio.whisper::whisper_languages()`.
+#' @param stream An optional number indicating the index of the audio stream in
+#'   `infile` to convert or extract. Note that ffmpeg uses zero-indexing so the
+#'   default of 0 is the first stream (default = 0).
+#' @param wavfile Either NULL or a string indicating the path to save the
+#'   prepared version of `infile` to (must end with '.wav'). If NULL, a
+#'   temporary file will be created and later discarded.
+#' @param rdsfile Either NULL or a string indicating the path to save the full
+#'   whisper output list object to (must end with '.rds').
+#' @param csvfile Either NULL or a string indicating the path to save a
+#'   human-readable version of the transcript to (must end with '.csv').
+#' @param ... Optional arguments to forward to
+#'   `audio.whisper:::predict.whisper()`.
+#' @return A list object containing the full whisper output.
+#' @export
+whisper_transcribe <- function(infile,
+                               model,
+                               language = "auto",
+                               stream = 0,
+                               wavfile = NULL,
+                               rdsfile = NULL,
+                               csvfile = NULL,
+                               ...) {
+
+  temp <- FALSE
+  if (whisper_check_audio(infile) == FALSE) {
+    # If no wavfile provided, create tempfile
+    if (is.null(wavfile)) {
+      temp <- TRUE
+      wavfile <- tempfile(fileext = ".wav")
+    }
+    # Prepare audio stream as wavfile/tempfile
+    x <- whisper_prepare_audio(
+      infile = infile,
+      outfile = wavfile,
+      stream = stream,
+      overwrite = overwrite
+    )
+  }
+
+  # Transcribe prepared audio file
+  out <- whisper_transcribe_wav(
+    infile = wavfile,
+    model = model,
+    language = language,
+    rdsfile = rdsfile,
+    csvfile = csvfile,
+    ...
+  )
+
+  # Clean up temporary file if created
+  if (temp) unlink(wavfile)
+
+  # Return the output from whisper
+  out
+}
+
+# whisper_transcribe_wav() ------------------------------------------------
+
+whisper_transcribe_wav <- function(infile,
+                                   model,
+                                   language = "auto",
+                                   rdsfile = NULL,
+                                   csvfile = NULL,
+                                   ...) {
+
+  stopifnot(is.character(infile) && length(infile) == 1 && file.exists(infile))
+  stopifnot(class(model) == "whisper")
+  stopifnot(is.character(language) && length(language) == 1)
+  stopifnot(is.null(rdsfile) || (is.character(rdsfile) && length(rdsfile) == 1 && tools::file_ext(rdsfile) == ".rds"))
+  stopifnot(is.null(csvfile) || (is.character(csvfile) && length(csvfile) == 1 && tools::file_ext(csvfile) == ".csv"))
+
+  out <- predict(
+    object = model,
+    newdata = infile,
+    type = "transcribe",
+    language = language,
+    ...
+  )
+
+  if (!is.null(rdsfile)) {
+    saveRDS(out, file = rdsfile)
+  }
+
+  if (!is.null(csvfile)) {
+    write.csv(out$data, file = csvfile)
+  }
+
+  out
+}
+
+
+
+
