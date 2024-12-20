784f11abce6a7e1120fd79eb73f5f751838253c2
jmgirard <me@jmgirard.com>
2024-12-20 10:30:10 -0600

    reexport future::plan()

---
+++ R/reexports.R
@@ -0,0 +1,13 @@
+#' Re-export the plan function from the future package
+#'
+#' This function is re-exported from the **future** package to allow users
+#' to set up and control the parallelization strategy directly.
+#'
+#' For more information, see the original documentation in the **future** package.
+#'
+#' @importFrom future plan
+#' @export
+#' @name plan
+#' @rdname plan
+#' @seealso \code{\link[future]{plan}}
+NULL
